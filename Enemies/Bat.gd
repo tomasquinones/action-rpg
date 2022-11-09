f3e9f4a7f4564cfea4cc37f47a373b6dc54724c6
tomasquinones <tomas.quinones@gmail.com>
2022-11-08 18:47:03 -0800

    soft collisions, no more bat-overlap

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -21,6 +21,7 @@ onready var sprite = $AnimatedSprite
 onready var stats = $Stats
 onready var playerDetectionZone = $PlayerDetectionZone
 onready var hurtbox = $HurtBox
+onready var softCollision = $SoftCollision
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
@@ -42,6 +43,10 @@ func _physics_process(delta):
 			else:
 				state = IDLE
 			sprite.flip_h = velocity.x < 0
+			
+	if softCollision.is_colliding():
+		velocity += softCollision.get_push_vector() * delta * 400
+		
 	velocity = move_and_slide(velocity)
 	
 	
